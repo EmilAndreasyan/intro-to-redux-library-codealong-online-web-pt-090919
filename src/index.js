5b321ce332a4f009de24f37cb91aab3d25dfdced
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-06-08 03:11:14 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -8,6 +8,6 @@ import shoppingListItemReducer from './reducers/shoppingListItemReducer.js';
 import App from './App';
 import './index.css';
 
-const store = createStore(shoppingListItemReducer)
+const store = createStore(shoppingListItemReducer, window.__REDUX_DEVTOOLS_EXTENSIONS__ && window.__REDUX_DEVTOOLS_EXTENSIONS__())
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
